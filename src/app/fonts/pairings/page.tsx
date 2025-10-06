67f55c0eb9081aad2d4387d34197c980967a35d0
Vahagn Vardanyan <vahagn.vardanyan2@picsart.com>
2025-10-06 23:45:13 +0400

    redirects to newly created fonts pages

---
+++ src/app/fonts/pairings/page.tsx
@@ -0,0 +1,7 @@
+import { redirect } from 'next/navigation';
+
+const Page = () => {
+  redirect('/texts/fonts/pairings');
+};
+
+export default Page;
---
+++ src/app/fonts/preview/page.tsx
@@ -0,0 +1,7 @@
+import { redirect } from 'next/navigation';
+
+const Page = () => {
+  redirect('/texts/fonts/preview');
+};
+
+export default Page;
