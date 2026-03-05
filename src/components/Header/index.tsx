8c6886978053e1f93e661a81253a9bf9c5e910ca
vahagnvardanyan1 <vahagn.v.vardanyan@gmail.com>
2026-03-05 19:32:12 +0400

    feat: Modify the necessary components or styles to updat

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -85,7 +85,7 @@ export const Header = () => {
   };
 
   return (
-    <header className="bg-white border-b border-gray-100 sticky top-0 z-50">
+    <header className="bg-red-500 border-b border-gray-100 sticky top-0 z-50">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-16 gap-2">
           {/* Logo */}
